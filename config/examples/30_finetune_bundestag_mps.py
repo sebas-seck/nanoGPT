c6c63d5da230ada93b11f259ff5335cdc2241790
Sebastian S Seck <Sebastian.S.Seck@deutschebahn.com>
2023-08-05 18:12:34 +0200

    more config updates

---
+++ config/examples/30_finetune_bundestag_mps.py
@@ -0,0 +1,29 @@
+import time
+
+out_dir = 'out-bundestag/30'
+eval_interval = 5
+eval_iters = 40
+wandb_log = True
+wandb_project = 'bundestag'
+wandb_run_name = 'tune-bt-gpt2-medium-ex30'
+
+dataset = 'bundestag'
+init_from = 'gpt2-medium' # this is the second smallest GPT-2 model
+
+# only save checkpoints if the validation loss improves
+always_save_checkpoint = False
+
+# the number of examples per iter:
+# 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
+# shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
+batch_size = 1
+gradient_accumulation_steps = 32
+max_iters = 20
+
+# finetune at constant LR
+learning_rate = 0.1
+decay_lr = True
+
+import torch
+device = 'mps' if  torch.backends.mps.is_available() else 'cpu'
+compile = False # do not torch compile the model
